e2047e62fff3ca50463e0b39778816af8cfd5937
zhvrnkv <v.zhvrnkv@gmail.com>
2019-08-17 01:40:20 +0600

    Make 5th exercise more inductable

--- chapter1/all-numbers-is-prime-numbers-production.hs
+++ chapter1/all-numbers-is-prime-numbers-production.hs
@@ -1,10 +1,7 @@
-primeNumbers = [0,1,3,5,7,11]
-isPrime n = n `elem` primeNumbers
-
 p :: Int -> Bool
+p 0 = True
+p 1 = True
 p 2 = True
-p n = if isPrime n
-      then True
-      else let f = n `div` 2
-      	       s = (*2) . mod n $ 2
-	   in (p f) && (p s)
+p n = let f = n `div` 2
+          s = (*2) . mod n $ 2
+      in (p f) && (p s)

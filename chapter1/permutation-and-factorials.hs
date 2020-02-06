00b39806e5f4d7086b05b45c416fbafd9256f7cc
zhvrnkv <v.zhvrnkv@gmail.com>
2020-02-06 22:55:19 +0600

    Implement "Method 2" from "1.2.5 Permutation and factorials"

--- chapter1/permutation-and-factorials.hs
+++ chapter1/permutation-and-factorials.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 data Permutation = Permutation { n :: [Int], k :: Int }
 
 count :: Permutation -> Int
@@ -8,15 +10,26 @@ count (Permutation objects k)
   | otherwise = (* n) . count $ Permutation (tail objects) (k - 1)
   where n = length objects
 
-allcombinations :: [a] -> [[a]]
+allcombinations :: Int -> [[Int]]
+allcombinations 0 = []
+allcombinations n = _allcombinations n [[1]]
 
-allcombinations [] = [[]]
-allcombinations objects =
-  (objects >>= (\object -> map (object:) . allcombinations . tail $ objects))
+_allcombinations :: Int -> [[Int]] -> [[Int]]
+_allcombinations counter perm
+  | counter == 1 = perm
+  | otherwise = _allcombinations (counter - 1) $ foldl (\acc xs -> acc ++ algorithm xs) [] $ perm
 
-ac :: [a] -> [[a]]
+algorithm :: [Int] -> [[Int]]
+algorithm objects = algorithm_new tuples bs
+  where tuples = algorithm_tuples objects
+        bs     = algorithm_bs [1..(length objects + 1)] tuples
 
-ac [] = [[]]
-ac os =
-  map t os
-  where t = \o -> map (o:) . ac . tail $ os
+algorithm_tuples :: [Int] -> [[Int]]
+algorithm_tuples perm = map (\k -> perm ++ [k]) [1..(length perm + 1)]
+
+algorithm_bs :: [Int] -> [[Int]] -> [[Int]]
+algorithm_bs ns tuples = map (\tuple -> filter (/= (last tuple)) ns) tuples
+
+algorithm_new :: [[Int]] -> [[Int]] -> [[Int]]
+algorithm_new tuples bs = map action $ zip tuples bs
+  where action = \(tuple, b) -> (map (\i -> b !! (i - 1)) (init tuple)) ++ [last tuple]
